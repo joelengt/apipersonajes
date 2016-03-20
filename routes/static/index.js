54f7d87c07667e0f4969f2234c9ed07d71ff9e5f
Joel Gonzales Tipismana <joelengt@gmail.com>
2016-03-20 16:03:07 -0500

    Aplicacion lista para deploy en heroku

---
+++ routes/static/index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var fs = require('fs')
+var app = express.Router()
+
+app.get('/demo', function (req, res) {
+	fs.readFile('./public/demo.html', function (err, data) {
+		if(err) {
+			return console.log(err)
+		}
+		res.send(data.toString())
+	})
+})
+
+module.exports = app
